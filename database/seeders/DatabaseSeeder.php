8ff80d1370d8ebce7b3b362e00f019bf5f6eccaa
antoniusekoputranto <antoniusekoputranto987@gmail.com>
2025-07-15 14:13:11 +0700

    Initial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Membuat 10 user secara acak
         // User::factory(10)->create();
 
+        // Membuat satu user dengan data spesifik
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('password123'), // Ganti dengan password yang aman
         ]);
     }
 }
